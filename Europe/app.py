2cc8b49b34e2d6522b55ee51641cb819ed9fcd62
alysedesouza <166676306+alysedesouza@users.noreply.github.com>
2024-06-06 13:38:46 +0930

    Update app.py

--- Europe/app.py
+++ Europe/app.py
@@ -69,4 +69,6 @@ def upload_file():
         return 'Invalid file format'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use the PORT environment variable provided by Heroku
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
