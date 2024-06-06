8370672daa02ba1be4974146c2597c525b9bf080
alysedesouza <166676306+alysedesouza@users.noreply.github.com>
2024-06-06 13:08:57 +0930

    Update app.py

--- Europe/app.py
+++ Europe/app.py
@@ -69,5 +69,6 @@ def upload_file():
     else:
         return 'Invalid file format'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
